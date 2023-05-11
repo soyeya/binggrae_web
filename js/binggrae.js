80beec4d1841753a020aaa743da7c1c173c02776
박소예 <soye@bagsoyeui-MacBookAir.local>
2023-05-11 18:45:58 +0900

    이미지 수정 및 반응형 작업

--- js/binggrae.js
+++ js/binggrae.js
@@ -2,18 +2,18 @@
 //header
 
 const binggrae_header = document.querySelector("header");
-
+const binggrae_nav = document.querySelector("nav");
+const binggrae_navList = document.querySelectorAll(".binggrae_category ul");
 
 window.addEventListener("scroll" , function(){
 
 
   var binggrae_scrollY = window.scrollY;
-  console.log(binggrae_scrollY)
 
 
-  if(binggrae_scrollY > 10){
+if(binggrae_scrollY > 10){
 
-    binggrae_header.classList.add("active");
+  binggrae_header.classList.add("active");
 
 
   }else{
@@ -25,6 +25,119 @@ window.addEventListener("scroll" , function(){
 
 })
 
+
+//nav
+
+function binggrae_menu(){
+
+
+binggrae_navList[0].addEventListener ("mouseover" , function(){
+ 
+  binggrae_nav.classList.add("active");
+ 
+  });
+binggrae_navList[0].addEventListener ("mouseout" , function(){
+ 
+  binggrae_nav.classList.remove("active");
+ 
+  });
+
+binggrae_navList[1].addEventListener ("mouseover" , function(){
+ 
+  binggrae_nav.classList.add("active");
+ 
+  });
+binggrae_navList[1].addEventListener ("mouseout" , function(){
+ 
+  binggrae_nav.classList.remove("active");
+ 
+  });
+
+binggrae_navList[2].addEventListener ("mouseover" , function(){
+ 
+  binggrae_nav.classList.add("active");
+ 
+  });
+binggrae_navList[2].addEventListener ("mouseout" , function(){
+ 
+  binggrae_nav.classList.remove("active");
+ 
+  });
+
+binggrae_navList[3].addEventListener ("mouseover" , function(){
+ 
+  binggrae_nav.classList.add("active");
+ 
+  });
+binggrae_navList[3].addEventListener ("mouseout" , function(){
+ 
+  binggrae_nav.classList.remove("active");
+ 
+  });
+
+binggrae_navList[4].addEventListener ("mouseover" , function(){
+ 
+  binggrae_nav.classList.add("active");
+ 
+  });
+binggrae_navList[4].addEventListener ("mouseout" , function(){
+ 
+  binggrae_nav.classList.remove("active");
+ 
+  });
+
+binggrae_navList[5].addEventListener ("mouseover" , function(){
+ 
+  binggrae_nav.classList.add("active");
+ 
+  });
+binggrae_navList[5].addEventListener ("mouseout" , function(){
+ 
+  binggrae_nav.classList.remove("active");
+ 
+  });
+
+}
+
+
+binggrae_menu();
+
+
+binggrae_nav.addEventListener("mouseover" , function(ev){
+
+  this.classList.add("active");
+
+  
+})
+
+binggrae_nav.addEventListener("mouseout" , function(ev){
+
+  this.classList.remove("active");
+
+  
+})
+
+//mobilbe_menu
+
+const mobile_menu = document.querySelector(".mobile_menu");
+
+var mobile_click = false;
+
+mobile_menu.addEventListener("click" , function(){
+
+  mobile_click =! mobile_click;
+
+  if(mobile_click == true){
+ 
+  binggrae_nav.classList.add("active");
+
+  }else{
+
+  binggrae_nav.classList.remove("active");
+
+  }
+
+})
 
 //search
 
